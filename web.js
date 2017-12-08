da0aef3681d7a8f32f9aceabf5fbf182af38f306
Dean Radcliffe <deanmisc@yahoo.com>
2017-12-08 10:23:04 -0600

    Update deps

--- web.js
+++ web.js
@@ -1,21 +1,16 @@
-var async    = require('async');
-var express  = require('express');
-var util     = require('util');
-var fs       = require('fs');
-var http     = require('http');
-var readdirp = require('readdirp'); 
-var chomp    = require('chomp');
-var rest     = require('restler');
-var AdmZip = require('adm-zip');
+var async = require("async");
+var express = require("express");
+var util = require("util");
+var fs = require("fs");
+var http = require("http");
+var readdirp = require("readdirp");
+var chomp = require("chomp");
+var rest = require("restler");
+var AdmZip = require("adm-zip");
 
 // create an express webserver
-var app = express.createServer(
-  express.logger(),
-  express.static(__dirname + '/public'),
-  express.bodyParser(),
-  express.cookieParser(),
-  // set this to a secret value to encrypt session cookies
-  express.session({ secret: process.env.SESSION_SECRET || 'secret123' })
+var app = express(
+  express.static(__dirname + "/public")
   /*,
   require('faceplate').middleware({
     app_id: process.env.FACEBOOK_APP_ID,
@@ -23,6 +18,7 @@ var app = express.createServer(
     scope:  'user_likes,user_photos,user_photo_video_tags'
   }) */
 );
+var helpers = require("express-helpers")(app);
 
 // listen to the PORT given to us in the environment
 var port = process.env.PORT || 3000;
@@ -31,116 +27,179 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-app.get('/', handle_facebook_request);
-app.post('/', handle_facebook_request);
-app.get('/getzip.zip', handle_zipper)
+app.get("/", renderIt);
+app.post("/", renderIt);
+app.get("/getzip.zip", handle_zipper);
 
 //my custom 404 thing
-app.use(function(req, res, next){
-  var url = unescape(req.url).toLowerCase().replace(' ', '').slice(1);
+app.use(function(req, res, next) {
+  var url = unescape(req.url)
+    .toLowerCase()
+    .replace(" ", "")
+    .slice(1);
 
-  if( url.indexOf('running') > -1)        { res.redirect('/running.html'); }
-  else if( url.indexOf('joggling') > -1 ) { res.redirect('http://www2.brightroom.com/107440/5574'); }
-  else{
+  if (url.indexOf("running") > -1) {
+    res.redirect("/running.html");
+  } else if (url.indexOf("joggling") > -1) {
+    res.redirect("http://www2.brightroom.com/107440/5574");
+  } else {
     var matches = [];
-    readdirp( { root: './public' }, 
-      //on a directory entry 
-      function(entry){
-        if( entry.path.toLowerCase().indexOf( url ) > -1 ){
-          if( entry.path.indexOf('.appcache') > -1  || entry.path.indexOf('/_') > -1){
+    readdirp(
+      { root: "./public" },
+      //on a directory entry
+      function(entry) {
+        if (entry.path.toLowerCase().indexOf(url) > -1) {
+          if (
+            entry.path.indexOf(".appcache") > -1 ||
+            entry.path.indexOf("/_") > -1
+          ) {
             //dont show
-          }
-          else if( entry.path.indexOf('.youtube') > -1 ){
-            var content = fs.readFileSync('./public/' + entry.path, "UTF8").chomp();
-            matches.push( { name: entry.path, path: entry.path, source: content, mtime: entry.stat.mtime, subtype: "video/youtube" } );
-          }
-          else if( entry.path.indexOf('.s3') > -1 ){
-            var content = fs.readFileSync('./public/' + entry.path, "UTF8").chomp()
-            matches.push( { name: entry.path, path: entry.path, source: content, mtime: entry.stat.mtime } );
-          }
-          else{
-            matches.push( { name: entry.path, path: entry.path, source: '/'+entry.path, mtime: entry.stat.mtime } );
+          } else if (entry.path.indexOf(".youtube") > -1) {
+            var content = fs
+              .readFileSync("./public/" + entry.path, "UTF8")
+              .chomp();
+            matches.push({
+              name: entry.path,
+              path: entry.path,
+              source: content,
+              mtime: entry.stat.mtime,
+              subtype: "video/youtube"
+            });
+          } else if (entry.path.indexOf(".s3") > -1) {
+            var content = fs
+              .readFileSync("./public/" + entry.path, "UTF8")
+              .chomp();
+            matches.push({
+              name: entry.path,
+              path: entry.path,
+              source: content,
+              mtime: entry.stat.mtime
+            });
+          } else {
+            matches.push({
+              name: entry.path,
+              path: entry.path,
+              source: "/" + entry.path,
+              mtime: entry.stat.mtime
+            });
           }
         }
       },
-      // on completion  
-	  function (err, resp) {
-	      matches.sort( function(a,b){
-	        var acmp = a.name;
-	        var bcmp = b.name;
+      // on completion
+      function(err, resp) {
+        matches.sort(function(a, b) {
+          var acmp = a.name;
+          var bcmp = b.name;
           return acmp.localeCompare(bcmp);
-	      });
-        res.render('search.ejs', {title: "Search Results for " + url , matches: matches, layout: false, showFullNav: false, status: 200, url: req.url, path: req.url}); 
+        });
+        res.render("search.ejs", {
+          title: "Search Results for " + url,
+          matches: matches,
+          layout: false,
+          showFullNav: false,
+          status: 200,
+          url: req.url,
+          path: req.url
+        });
       }
     );
-
   }
 });
 
-app.helpers({
-  'render_match': function(m){
-    var audio = " <audio src=\"" + m.source + "\" type='audio/mp3' controls='controls' preload='metadata'></audio>";
-    var video = "  <video controls='controls' preload='metadata'><source src='" + m.source + "' type='video/youtube'></source></video>\n";
-    var incaudio = m.path.match( /\.mp3/i );
-    var incvideo = (m.subtype == "video/youtube");
+var helpers = {
+  render_match: function(m) {
+    var audio =
+      ' <audio src="' +
+      m.source +
+      "\" type='audio/mp3' controls='controls' preload='metadata'></audio>";
+    var video =
+      "  <video controls='controls' preload='metadata'><source src='" +
+      m.source +
+      "' type='video/youtube'></source></video>\n";
+    var incaudio = m.path.match(/\.mp3/i);
+    var incvideo = m.subtype == "video/youtube";
 
-	var disppath = (m.name.indexOf('s3') > -1) ? m.source : m.path ;
-	//var disppath = m.source;
-    var dispname = m.name.replace( /\.s3$/, '');
-    var dispshort = dispname.replace( /^.*\//, '');
-	
-    return " <li>\n" + 
-           "<span title=\"" + m.path + "\">" + dispshort + "</span>" +
-           "  <a title=\"" + disppath + "\" \nhref=\"" + m.source + "\"\n>Link To Source</a>\n" + 
-           (incaudio ? audio : "") + 
-           (incvideo ? video : "") + 
-           " </li>";
+    var disppath = m.name.indexOf("s3") > -1 ? m.source : m.path;
+    //var disppath = m.source;
+    var dispname = m.name.replace(/\.s3$/, "");
+    var dispshort = dispname.replace(/^.*\//, "");
+
+    return (
+      " <li>\n" +
+      '<span title="' +
+      m.path +
+      '">' +
+      dispshort +
+      "</span>" +
+      '  <a title="' +
+      disppath +
+      '" \nhref="' +
+      m.source +
+      '"\n>Link To Source</a>\n' +
+      (incaudio ? audio : "") +
+      (incvideo ? video : "") +
+      " </li>"
+    );
   }
-});
+};
 
+var dynamicHelpers = {
+  host: function(req, res) {
+    return req.headers["host"];
+  },
+  scheme: function(req, res) {
+    req.headers["x-forwarded-proto"] || "http";
+  },
+  url: function(req, res) {
+    return function(path) {
+      return (
+        app.dynamicViewHelpers.scheme(req, res) +
+        app.dynamicViewHelpers.url_no_scheme(path)
+      );
+    };
+  },
+  url_no_scheme: function(req, res) {
+    return function(path) {
+      return "://" + app.dynamicViewHelpers.host(req, res) + path;
+    };
+  }
+};
 
-app.dynamicHelpers({
-  'host': function(req, res) {
-    return req.headers['host'];
-  },
-  'scheme': function(req, res) {
-    req.headers['x-forwarded-proto'] || 'http'
-  },
-  'url': function(req, res) {
-    return function(path) {
-      return app.dynamicViewHelpers.scheme(req, res) + app.dynamicViewHelpers.url_no_scheme(path);
-    }
-  },
-  'url_no_scheme': function(req, res) {
-    return function(path) {
-      return '://' + app.dynamicViewHelpers.host(req, res) + path;
-    }
-  },
-});
+function renderIt(req, res) {
+  res.render("index.ejs", {
+    layout: false,
+    req: req,
+    app: app,
+    user: {}
+  });
+}
 
 function render_page(req, res) {
   req.facebook.app(function(app) {
     req.facebook.me(function(user) {
-      res.render('index.ejs', {
-        layout:    false,
-        req:       req,
-        app:       app,
-        user:      user
+      res.render("index.ejs", {
+        layout: false,
+        req: req,
+        app: app,
+        user: user
       });
     });
   });
 }
 
 function handle_zipper(req, res) {
-	var searchpath = req.query.path;
-	var url = unescape(searchpath).toLowerCase().replace(' ', '').slice(1);
-	
-	res.setHeader('Content-Type', 'text/html');
-	res.send(new Buffer('Coming soon: getting zip' + req.query.path));
-	
-	/*
+  var searchpath = req.query.path;
+  var url = unescape(searchpath)
+    .toLowerCase()
+    .replace(" ", "")
+    .slice(1);
+
+  res.setHeader("Content-Type", "text/html");
+  res.send(new Buffer("Coming soon: getting zip" + req.query.path));
+
+  /*
     var zip = new AdmZip();
-    readdirp( { root: './public' }, 
+    readdirp( { root: './public' },
       function(entry){
 		if( entry.path.toLowerCase().indexOf( url ) > -1 ){
 	      if( entry.path.indexOf('.mp3.s3') > -1 ){
@@ -162,7 +221,7 @@ function handle_zipper(req, res) {
           }
 		}
       },
-      // on completion  
+      // on completion
 	  function (err, resp) {
 		console.log("sending zip");
 		res.setHeader('Content-Type', 'application/zip');
@@ -170,50 +229,50 @@ function handle_zipper(req, res) {
         res.send(zip.toBuffer());
       }
     );	*/
-	
 }
 
 function handle_facebook_request(req, res) {
-
   // if the user is logged in
   if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
+    async.parallel(
+      [
+        function(cb) {
+          // query 4 friends and send them to the socket for this socket id
+          req.facebook.get("/me/friends", { limit: 4 }, function(friends) {
+            req.friends = friends;
+            cb();
+          });
+        },
+        function(cb) {
+          // query 16 photos and send them to the socket for this socket id
+          req.facebook.get("/me/photos", { limit: 16 }, function(photos) {
+            req.photos = photos;
+            cb();
+          });
+        },
+        function(cb) {
+          // query 4 likes and send them to the socket for this socket id
+          req.facebook.get("/me/likes", { limit: 4 }, function(likes) {
+            req.likes = likes;
+            cb();
+          });
+        },
+        function(cb) {
+          // use fql to get a list of my friends that are using this app
+          req.facebook.fql(
+            "SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1",
+            function(result) {
+              req.friends_using_app = result;
+              cb();
+            }
+          );
+        }
+      ],
+      function() {
+        render_page(req, res);
       }
-    ], function() {
-      render_page(req, res);
-    });
-
+    );
   } else {
     render_page(req, res);
   }
 }
-
-
